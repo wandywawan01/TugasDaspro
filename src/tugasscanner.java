27ff5a599737524671330fa1c3d18b960fef31a2
wandywawan01 <56427382+wandywawan01@users.noreply.github.com>
2019-10-11 13:59:44 +0700

    Add files via upload

---
+++ src/tugasscanner.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+
+public class tugasscanner {
+
+    public static void main(String[]args){
+        
+        Scanner inputUser = new Scanner(System.in);
+        // riwayat pendidikan
+       
+        System.out.print("riwayat pendidikan");
+        String wandy = inputUser.nextLine();
+        
+        System.out.println("nama saya"+wandy ); 
+        
+        System.out.print("saya menempuh sekolah dasar di");
+        String SDN145 = inputUser.nextLine();
+        
+        System.out.println("dari sekolah: "+SDN145);
+        
+        
+        System.out.print("saya menempuh sekolah menengah di");
+        
+        String SMPN22 = inputUser.nextLine();
+        
+        System.out.println("dari sekolah:"+SMPN22);
+        
+        System.out.println("saya menempuh sekolah menengah atas di");
+        
+        String SMKMASMUR = inputUser.nextLine();
+        
+        System.out.println("dari sekolah:"+SMKMASMUR);
+        
+        
+        System.out.println("dan sekarang saya berkuliah di");
+        
+        String uinsuskariau = inputUser.nextLine();
+        
+        System.out.println("sedang menempuh pendidikan:"+uinsuskariau);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+       
+        
+        
+        
+    }
+}
